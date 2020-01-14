7fb86bb935f4c377171a69f11df78b2c3b6f3e41
extraname <zorraider@gmail.com>
2020-01-14 13:53:46 +0200

    low refactoring

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ api.add_resource(DashBoardTasks, '/dashboards/<int:dashboard_id>/tasks')
 
 api.add_resource(Tasks, '/tasks')
 api.add_resource(SingleTask, '/tasks/<int:task_id>')
-api.add_resource(TaskMasterUser, '/tasks/<int:tasks_id>/master')
+api.add_resource(TaskMasterUser, '/tasks/<int:task_id>/master')
 api.add_resource(TaskUsers, '/tasks/<int:task_id>/users')
 api.add_resource(TaskComment, '/tasks/<int:task_id>/comment')
 api.add_resource(TaskStatus, '/tasks/<int:task_id>/status')
